3bffcf92336c79164cb3f84e34ef4b0c15cd7c52
muhamadherwan <hello.wanlab@gmail.com>
2022-04-06 16:42:02 +0800

    update

--- responsive1/script.js
+++ responsive1/script.js
@@ -1,3 +1,4 @@
+// menu slide popup
 var menuBtn = document.getElementById("menuBtn");
 var sideNav = document.getElementById("sideNav");
 
@@ -7,3 +8,9 @@ menuBtn.onclick = () => {
     ? (sideNav.style.right = "0")
     : (sideNav.style.right = "-250px");
 };
+
+// menu link smooth scroll
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
